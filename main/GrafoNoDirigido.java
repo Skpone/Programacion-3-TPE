de9fd20d5205e81a1c49514103aed2cdd933d509
Skpone <mulaterofranco@gmail.com>
2023-05-09 21:59:17 -0300

    .

--- main/GrafoNoDirigido.java
+++ main/GrafoNoDirigido.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package main;
 
-/**
- *
- * @author mulat
- */
+
+
 public class GrafoNoDirigido<T> extends GrafoDirigido<T>{
     @Override
 	public void agregarArco(int verticeId1, int verticeId2, T etiqueta) {
