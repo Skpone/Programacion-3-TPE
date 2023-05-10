541869c827d0386cda7880b5ea31e256bc3e103c
Franco Mulatero <72869504+Skpone@users.noreply.github.com>
2023-05-09 21:40:48 -0300

    archivos
    archivos a utilizar para primera parte

---
+++ servicios/ServicioDFS.java
@@ -0,0 +1,21 @@
+package servicios;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import estructuras.Grafo;
+
+public class ServicioDFS {
+
+	private Grafo<?> grafo;
+
+	public ServicioDFS(Grafo<?> grafo) {
+		this.grafo = grafo;
+	}
+	
+	public List<Integer> dfsForest() {
+		// Resolver DFS
+		return new ArrayList<>();
+	}
+
+}
