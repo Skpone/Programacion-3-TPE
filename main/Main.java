52b45a3167cabaad0713acece32e434a452d8fbe
Skpone <mulaterofranco@gmail.com>
2023-06-20 15:45:24 -0300

    UnionFind.java agregado

--- main/Main.java
+++ main/Main.java
@@ -2,12 +2,12 @@ package main;
 
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		String path = "../datasets";
-		CSVReader reader = new CSVReader(path);
-		reader.read();
-		
-	}
+        String path = "src/datasets/dataset1.txt";
+        CSVReader reader = new CSVReader(path);
+        reader.read();
+
+    }
 
 }
